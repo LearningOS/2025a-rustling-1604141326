16e8baee8a0d3e455ecf4d694e291008aeba4f6d
Admin <1604141326@qq.com>
2025-09-23 18:13:06 +0800

    6

--- exercises/algorithm/algorithm6.rs
+++ exercises/algorithm/algorithm6.rs
@@ -23,7 +23,16 @@ impl Graph {
     }
 
     fn dfs_util(&self, v: usize, visited: &mut HashSet<usize>, visit_order: &mut Vec<usize>) {
-        //TODO
+        // Mark the current node as visited and add it to visit order
+        visited.insert(v);
+        visit_order.push(v);
+        
+        // Recur for all adjacent vertices that haven't been visited
+        for &neighbor in &self.adj[v] {
+            if !visited.contains(&neighbor) {
+                self.dfs_util(neighbor, visited, visit_order);
+            }
+        }
     }
 
     // Perform a depth-first search on the graph, return the order of visited nodes
@@ -75,4 +84,3 @@ mod tests {
         assert_eq!(visit_order_disconnected, vec![3, 4]); 
     }
 }
-
