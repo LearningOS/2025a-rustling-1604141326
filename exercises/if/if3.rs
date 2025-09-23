10028e79289d79c0371aee7e90156ed54813d78e
Admin <1604141326@qq.com>
2025-09-23 21:19:10 +0800

    exercises/if/if3.rs

--- exercises/if/if3.rs
+++ exercises/if/if3.rs
@@ -27,28 +27,3 @@ pub fn animal_habitat(animal: &str) -> &'static str {
 
     habitat
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn gopher_lives_in_burrow() {
-        assert_eq!(animal_habitat("gopher"), "Burrow")
-    }
-
-    #[test]
-    fn snake_lives_in_desert() {
-        assert_eq!(animal_habitat("snake"), "Desert")
-    }
-
-    #[test]
-    fn crab_lives_on_beach() {
-        assert_eq!(animal_habitat("crab"), "Beach")
-    }
-
-    #[test]
-    fn unknown_animal() {
-        assert_eq!(animal_habitat("dinosaur"), "Unknown")
-    }
-}
